e1e938dab31536b3fd2a847349c5d4c1291eaae8
mmalafaia <mmalafaia@gmail.com>
2019-10-14 16:34:50 -0300

    chore: Commit Inicial

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+
+const server = express();
+
+var reqCounts = 0;
+
+server.use(express.json());
+
+// Query Params = ?teste=1
+// Route Params = /users/1
+// Request body = { "name": "Manoel", "email": "mmalafaia@gmail.com"}
+
+// CRUD - Create, Read, Update and Delete
+
+const projects = [
+  {
+    id: "1",
+    title: "Projeto NodeJS",
+    tasks: ["Instalar Ambiente", "Configura Projeto"]
+  },
+  {
+    id: "2",
+    title: "Projeto ReactJS",
+    tasks: ["Configurar VSCode", "Rodar Demo"]
+  }
+];
+
+
+
+server.use((req, res, next) => {
+  reqCounts++;
+  next();
+
+  console.log(`Quantidade de Requisições: ${reqCounts}`);
+})
+
+function checkProjectInArray(req, res, next) {
+  const index = projects.findIndex(x => x.id === req.params.id);
+  const project = projects[index];
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project does not exists' });
+  }
+
+  req.project = project;
+  req.index = index;
+
+  return next();
+}
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+})
+
+server.get('/projects/:id', checkProjectInArray, (req, res) => {
+  return res.json(req.project);
+})
+
+server.post('/projects', (req, res) => {
+  const { project}  = req.body;
+
+  projects.push(project);
+
+  return res.json(projects);
+})
+
+server.put('/projects/:id', checkProjectInArray, (req, res) => {
+  const { title } = req.body.project;
+
+  projects[req.index].title = title;
+
+  return res.json(projects);
+})
+
+server.delete('/projects/:id', checkProjectInArray, (req, res) => {
+  projects.splice(req.index, 1);
+
+  return res.send();  
+})
+
+server.post('/projects/:id/tasks', checkProjectInArray, (req, res) => {
+  const { title }  = req.body;
+
+  projects[req.index].tasks.push(title);
+
+  return res.json(projects[req.index]);
+})
+
+server.listen(3000);
